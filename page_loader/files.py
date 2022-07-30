667ffb9e1152b244a3b9756d526fb5f654610fdd
Dmitrii Morozov <33morozov@gmail.com>
2022-07-31 00:16:55 +0300

    Add threading in function load.py. Update dist

--- page_loader/files.py
+++ page_loader/files.py
@@ -4,6 +4,7 @@ import logging
 from page_loader.names import get_name
 from urllib.parse import urlparse, urljoin
 from progress.bar import Bar
+from concurrent import futures
 
 
 def get_file(soup, url_, type_):
@@ -18,23 +19,31 @@ def get_file(soup, url_, type_):
     bar = Bar(f"Downloading {type_} files:",
               max=len(tags),
               suffix='%(index)d/%(max)d')
-    for tag in soup.find_all(type_):
-        if urlparse(tag.get(type_link)).netloc in (page_netloc, ''):
-            link = urljoin(url_, tag.get(type_link))
-            name = get_name(link)
-            path = os.path.join(os.getcwd(), name)
-            with open(path, type_file) as file:
-                try:
-                    download_file = requests.get(link)
-                    download_file.raise_for_status()
-                except IOError as request_err:
-                    logging.error(f"{request_err}")
-                    raise
-                else:
-                    if type_ == 'img':
-                        file.write(download_file.content)
-                    elif type_ in ('link', 'script'):
-                        file.write(download_file.text)
-            tag[type_link] = os.path.join(os.path.basename(os.getcwd()), name)
-            bar.next()
-    bar.finish()
+    with futures.ThreadPoolExecutor(max_workers=8) as executor, bar:
+        tasks = [executor.submit(load_save_file, tag, url_, type_,
+                                 type_link, type_file, page_netloc, bar
+                                 ) for tag in soup.find_all(type_)]
+        bar.finish()
+        result = [task.result() for task in tasks]
+        logging.info(f"All assets was downloaded: {result}")
+
+
+def load_save_file(tag, url_, type_, type_link, type_file, page_netloc, bar):
+    if urlparse(tag.get(type_link)).netloc in (page_netloc, ''):
+        link = urljoin(url_, tag.get(type_link))
+        name = get_name(link)
+        path = os.path.join(os.getcwd(), name)
+        with open(path, type_file) as file:
+            try:
+                download_file = requests.get(link)
+                download_file.raise_for_status()
+                bar.next()
+            except IOError as request_err:
+                logging.error(f"{request_err}")
+                raise
+            else:
+                if type_ == 'img':
+                    file.write(download_file.content)
+                elif type_ in ('link', 'script'):
+                    file.write(download_file.text)
+        tag[type_link] = os.path.join(os.path.basename(os.getcwd()), name)
